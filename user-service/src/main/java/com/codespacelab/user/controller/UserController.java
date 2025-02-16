ee4e2613a3164b2c9d620476caf9edeed894dfc1
Anastasov <anastasovdavid@gmail.com>
2025-02-16 16:11:19 +0100

    finished user-service implementation

--- user-service/src/main/java/com/codespacelab/user/controller/UserController.java
+++ user-service/src/main/java/com/codespacelab/user/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.codespacelab.user.controller;
 
 import com.codespacelab.user.model.dto.UserDTO;
+import com.codespacelab.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -13,39 +13,46 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController
 {
+    private final UserService userService;
+
+    public UserController(final UserService userService)
+    {
+        this.userService = userService;
+    }
+
     @GetMapping("/all")
     public List<UserDTO> getUsers()
     {
-        return new ArrayList<>();
+        return userService.getUsers();
     }
 
     @GetMapping
     public UserDTO getUser(@RequestParam(name = "id") Long id)
     {
-        return UserDTO.builder().build();
+        return userService.getUser(id);
     }
 
     @PostMapping
     public UserDTO addUser(@Validated @RequestBody UserDTO userDTO)
     {
-        return UserDTO.builder().build();
+        return userService.addUser(userDTO);
     }
 
     @PutMapping
     public UserDTO updateUser(@Validated @RequestBody UserDTO userDTO)
     {
-        return UserDTO.builder().build();
+        return userService.updateUser(userDTO);
     }
 
     @DeleteMapping
     public boolean deleteUser(@RequestParam(name = "id") Long id)
     {
-        return true;
+        return userService.deleteUser(id);
     }
 
     @GetMapping("/validate")
     public boolean validateUser(@RequestParam(name = "id") Long id)
     {
-        return true;
+        return userService.validateUser(id);
     }
 }
