0d0d92f54ed34f0803ce533c33a69caf7a1abe96
Anastasov <anastasovdavid@gmail.com>
2025-02-16 16:13:29 +0100

    order-service-module

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -18,7 +18,12 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework:spring-web:6.2.2")
+	implementation("com.fasterxml.jackson.core:jackson-annotations:2.18.2")
+	compileOnly("org.projectlombok:lombok:1.18.36")
+	annotationProcessor("org.projectlombok:lombok:1.18.36")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
