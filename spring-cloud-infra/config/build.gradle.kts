bdedcf4273e88b8a94104b2e5515912f84c7a613
Anastasov <anastasovdavid@gmail.com>
2025-02-16 21:14:43 +0100

    successfully started config server, retrieved data via registry and config

--- spring-cloud-infra/config/build.gradle.kts
+++ spring-cloud-infra/config/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.2"
+	id("org.springframework.boot") version "3.2.1"
 	id("io.spring.dependency-management") version "1.1.7"
+	java
 }
 
 group = "com.codespacelab"
@@ -15,12 +15,20 @@ java {
 
 repositories {
 	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	implementation("org.springframework.cloud:spring-cloud-config-server")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.2")
+	}
 }
 
 tasks.withType<Test> {
--- spring-cloud-infra/config/src/main/java/com/codespacelab/config/ConfigApplication.java
+++ spring-cloud-infra/config/src/main/java/com/codespacelab/config/ConfigApplication.java
@@ -2,11 +2,16 @@ package com.codespacelab.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
-public class ConfigApplication {
-
-	public static void main(String[] args) {
+public class ConfigApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ConfigApplication.class, args);
 	}
 
