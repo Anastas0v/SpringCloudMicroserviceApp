217176f4e5864b7773136d14224dca997f620494
Anastasov <anastasovdavid@gmail.com>
2025-02-16 17:04:56 +0100

    all working

--- spring-cloud-infra/build.gradle.kts
+++ spring-cloud-infra/build.gradle.kts
@@ -19,8 +19,14 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server:4.2.0")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
+	}
 }
 
 tasks.withType<Test> {
