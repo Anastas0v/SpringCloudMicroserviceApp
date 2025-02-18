66379fb6585b68b2d077759d86ebded3db2c5c94
Anastasov <anastasovdavid@gmail.com>
2025-02-18 22:08:15 +0100

    order service connected success

--- order-service/src/main/java/com/codespacelab/order/controller/OrderController.java
+++ order-service/src/main/java/com/codespacelab/order/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.codespacelab.order.controller;
 import com.codespacelab.order.model.dto.OrderDTO;
 import com.codespacelab.order.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,9 @@ import java.util.List;
 @RequestMapping("/order")
 public class OrderController
 {
+    @Value("${spring.datasource.url}")
+    private String database;
+
     private final OrderService orderService;
 
     public OrderController(final OrderService orderService)
@@ -23,6 +27,7 @@ public class OrderController
     @GetMapping("/all")
     public List<OrderDTO> getOrders()
     {
+        log.info("DataBase URL: " + database);
         return orderService.getOrders();
     }
 
